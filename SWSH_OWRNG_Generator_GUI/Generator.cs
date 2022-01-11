01e433c6d662824e4708489b54f6947cc7a4c58c
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2022-01-12 08:27:54 +1100

    Style changes

--- SWSH_OWRNG_Generator_GUI/Generator.cs
+++ SWSH_OWRNG_Generator_GUI/Generator.cs
@@ -57,7 +57,7 @@ namespace SWSH_OWRNG_Generator_GUI
                 if (Static)
                 {
                     LeadRand = (uint)rng.Rand(100);
-                    if (IsCuteCharm && LeadRand <= 65)
+                    if (IsCuteCharm && LeadRand < 66)
                         Gender = "CC";
                 }
                 else
@@ -66,7 +66,7 @@ namespace SWSH_OWRNG_Generator_GUI
                         rng.Rand();
                     rng.Rand(100);
                     LeadRand = (uint)rng.Rand(100);
-                    if (IsCuteCharm && LeadRand <= 65)
+                    if (IsCuteCharm && LeadRand < 66)
                         Gender = "CC";
 
                     SlotRand = (uint)rng.Rand(100);
@@ -174,32 +174,32 @@ namespace SWSH_OWRNG_Generator_GUI
 
                 // Passes all filters!
                 Results.Add(
-                new Frame()
-                {
-                    Advances = TIDSIDSearch ? (-(long)(advance + InitialAdvances)).ToString("N0") : (advance + InitialAdvances).ToString("N0"),
-                    TID = (ushort)(MockPID >> 16),
-                    SID = (ushort)MockPID,
-                    Animation = go.state0 & 1 ^ go.state1 & 1,
-                    Level = Level,
-                    Slot = SlotRand,
-                    PID = PID.ToString("X8"),
-                    EC = EC.ToString("X8"),
-                    Shiny = ShinyXOR == 0 ? "Square" : (ShinyXOR < 16 ? "Star" : "No"),
-                    Brilliant = Brilliant ? "Y" : "-",
-                    Ability = AbilityRoll == 0 ? 1 : 0,
-                    Nature = Natures[Nature],
-                    Gender = Gender,
-                    HP = IVs[0],
-                    Atk = IVs[1],
-                    Def = IVs[2],
-                    SpA = IVs[3],
-                    SpD = IVs[4],
-                    Spe = IVs[5],
-                    Mark = Mark,
-                    State0 = go.state0.ToString("X16"),
-                    State1 = go.state1.ToString("X16"),
-                }
-            );
+                    new Frame()
+                    {
+                        Advances = TIDSIDSearch ? (-(long)(advance + InitialAdvances)).ToString("N0") : (advance + InitialAdvances).ToString("N0"),
+                        TID = (ushort)(MockPID >> 16),
+                        SID = (ushort)MockPID,
+                        Animation = go.state0 & 1 ^ go.state1 & 1,
+                        Level = Level,
+                        Slot = SlotRand,
+                        PID = PID.ToString("X8"),
+                        EC = EC.ToString("X8"),
+                        Shiny = ShinyXOR == 0 ? "Square" : (ShinyXOR < 16 ? "Star" : "No"),
+                        Brilliant = Brilliant ? "Y" : "-",
+                        Ability = AbilityRoll == 0 ? 1 : 0,
+                        Nature = Natures[Nature],
+                        Gender = Gender,
+                        HP = IVs[0],
+                        Atk = IVs[1],
+                        Def = IVs[2],
+                        SpA = IVs[3],
+                        SpD = IVs[4],
+                        Spe = IVs[5],
+                        Mark = Mark,
+                        State0 = go.state0.ToString("X16"),
+                        State1 = go.state1.ToString("X16"),
+                    }
+                );
 
                 if (TIDSIDSearch)
                     go.Previous();
