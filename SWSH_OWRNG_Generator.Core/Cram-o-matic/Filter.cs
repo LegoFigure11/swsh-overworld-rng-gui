bdf477c94018a8d0afd8018a33945cff6d041940
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2022-06-01 00:30:01 +1000

    Cleanup Filter.cs

--- SWSH_OWRNG_Generator.Core/Cram-o-matic/Filter.cs
+++ SWSH_OWRNG_Generator.Core/Cram-o-matic/Filter.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SWSH_OWRNG_Generator.Core.Cram_o_matic
+﻿namespace SWSH_OWRNG_Generator.Core.Cram_o_matic
 {
     public class Filter
     {
