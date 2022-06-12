8351725d481af6d880b228a33fe4326d14e6ba2b
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2022-06-12 23:37:22 +1000

    Make MenuClose.GetAdvances() implement Advance()

--- SWSH_OWRNG_Generator.Core/MenuClose/Generator.cs
+++ SWSH_OWRNG_Generator.Core/MenuClose/Generator.cs
@@ -17,12 +17,7 @@ namespace SWSH_OWRNG_Generator.Core.MenuClose
         public static uint GetAdvances(Xoroshiro128Plus rng, uint NPCs)
         {
             (ulong _s0, ulong _s1) = rng.GetState();
-            for (uint i = 0; i < NPCs; i++)
-            {
-                rng.NextInt(91);
-            }
-            rng.Next();
-            rng.NextInt(60);
+            Advance(ref rng, NPCs);
 
             uint c = 0;
             while (c < 500) // Prevent infinite loop in case we never find our starting position
