2eac5e4f8d0e4ebdf5c0c306f50fcf1979608c45
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2022-09-06 11:53:49 +1000

    Increase search limit from int to long

--- SWSH_OWRNG_Generator.WinForms/MainWindow.cs
+++ SWSH_OWRNG_Generator.WinForms/MainWindow.cs
@@ -145,7 +145,7 @@ namespace SWSH_OWRNG_Generator.WinForms
         private void SetToZero_LostFocus(object sender, EventArgs e)
         {
             TextBox textBox = (TextBox)sender;
-            if (int.TryParse(textBox.Text, out int i))
+            if (long.TryParse(textBox.Text, out long i))
             {
                 if (i < 0) textBox.Text = "0";
             }
