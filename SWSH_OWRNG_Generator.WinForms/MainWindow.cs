1afd50fea5b431cee220f080e7ac9ac34731e08c
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2022-10-27 20:29:09 +1100

    Bump Deps

--- SWSH_OWRNG_Generator.WinForms/MainWindow.cs
+++ SWSH_OWRNG_Generator.WinForms/MainWindow.cs
@@ -1155,7 +1155,7 @@ namespace SWSH_OWRNG_Generator.WinForms
                 if (pk.Species > 0 && pk.Species <= 899)
                 {
                     TextboxSetText(Program.Window.TextBoxCheckEncounter, output);
-                    var img = SpriteUtil.GetSprite(pk.Species, pk.Form, pk.Gender, pk.FormArgument, pk.HeldItem, false, pk.IsShiny, 8, false, isSquare);
+                    var img = pk.Sprite();
                     PokeSprite.Image = img;
                     if (hasMark)
                     {
